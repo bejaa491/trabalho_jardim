769d15f4ff36a4ce921b54624b9460b4e424ab95
bejaa491 <rodrigobeja491@gmail.com>
2025-10-23 14:11:43 +0100

    fix a algumas coisas

--- Tesoura.cpp
+++ Tesoura.cpp
@@ -7,11 +7,11 @@ Tesoura::~Tesoura() {}
 
 bool Tesoura::usar(Posicao* pos) {
     Planta* p = pos->getPlanta();
-    if (p != nullptr) {
+    if (p != nullptr && p->getBeleza() == Beleza::FEIA) {
         delete p;
         pos->removerPlanta();
     }
-    return false;
+    return false; // Nunca se destrói
 }
 
 char Tesoura::getCaracter() const { return 'T'; }
