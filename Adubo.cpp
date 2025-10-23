3e51b2cf001debd3b0d1eed432907668935db12f
Joao Marques <jmarquesufc@gmail.com>
2025-10-23 18:44:02 +0100

    Revert "Merge branch 'main' of https://github.com/bejaa491/trabalho_jardim"
    This reverts commit 141ce2012e45cfa66c6532b1016ceddf0130cb8b, reversing
    changes made to 77ddf146389ecf0e522d7c02782058b8e8ab911f.

--- Adubo.cpp
+++ Adubo.cpp
@@ -1,11 +1,3 @@
-#include "Adubo.h"
-
-int quantidadeAdubo; // 100 inicialmente
-bool usar(Posicao* pos) {
-    if (quantidadeAdubo >= 10) {
-        pos->adicionarNutrientes(10);
-        quantidadeAdubo -= 10;
-        return false;
-    }
-    return true; // Destruir quando vazio
-}
+//
+// Created by rodri on 22/10/2025.
+//
--- Tesoura.cpp
+++ Tesoura.cpp
@@ -7,11 +7,11 @@ Tesoura::~Tesoura() {}
 
 bool Tesoura::usar(Posicao* pos) {
     Planta* p = pos->getPlanta();
-    if (p != nullptr && p->getBeleza() == Beleza::FEIA) {
+    if (p != nullptr) {
         delete p;
         pos->removerPlanta();
     }
-    return false; // Nunca se destrói
+    return false;
 }
 
 char Tesoura::getCaracter() const { return 'T'; }
